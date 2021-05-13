e4aa4c0286bfdb5de56952726197f857a39f15bf
Chelsea Laguma <chelseal@globalkinetic.com>
2021-05-13 15:58:15 +0200

    Setting up BaseTest and SampleTest

--- src/test/java/playground/SampleTest.java
+++ src/test/java/playground/SampleTest.java
@@ -4,15 +4,9 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
-import java.util.Random;
-
 
 public class SampleTest extends BaseTest {
-
 
     @Test
     public void goToWomensPage() {
@@ -22,32 +16,15 @@ public class SampleTest extends BaseTest {
         Assert.assertEquals(title, "My Store");
     }
 
-
-
     @Test
-    public void addProductToWishListTest() {
-        registerNewUser();
-        addProductToWishList();
-
-    }
-    @Test
-    public void registerNewUserTest(){
-
-        registerNewUser();
-    }
-
-    @Test
-    public void shareWishListTest(){
+    public void shareWishListTest() {
         registerNewUser();
         addProductToWishList();
         shareWishList();
-
     }
 
     public void registerNewUser() {
-
         String title = driver.getTitle();
-
         driver.findElement(By.xpath("//*[@id=\"header\"]//*[@class=\"skip-links\"]//*[@data-target-element=\"#header-account\"]")).click();
         driver.findElement(By.linkText("My Account")).click();
         driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/div[1]/div[2]/a")).click();
@@ -59,26 +36,20 @@ public class SampleTest extends BaseTest {
         driver.findElement(By.name("password")).sendKeys("chelsea");
         driver.findElement(By.name("confirmation")).sendKeys("chelsea");
         driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button")).click();
-
         Assert.assertEquals(title, "Home page");
+
     }
 
     public void addProductToWishList() {
-        String title = driver.getTitle();
         driver.findElement(By.xpath("//*[@id=\"nav\"]/ol/li[2]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[2]/ul/li[2]/div/div[3]/ul/li[1]/a")).click();
     }
 
-    public void shareWishList(){
-        String title = driver.getTitle();
+    public void shareWishList() {
         driver.findElement(By.xpath("//*[@id=\"wishlist-view-form\"]/div/div/button[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"email_address\"]")).sendKeys("chelslaguma");
+        driver.findElement(By.xpath("//*[@id=\"email_address\"]")).sendKeys("chelslaguma@gmail.com");
         driver.findElement(By.xpath("//*[@id=\"message\"]")).sendKeys("I love this tv we should buy it ");
         driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button")).click();
 
-
     }
-
-
-
 }
