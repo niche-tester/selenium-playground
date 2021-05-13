164ceec85f8688a601d0d5cab09e677a476e45e3
Chelsea Laguma <chelseal@globalkinetic.com>
2021-05-13 14:56:26 +0200

    Setting up BaseTest and SampleTest

--- src/test/java/playground/BaseTest.java
+++ src/test/java/playground/BaseTest.java
@@ -2,30 +2,36 @@ package playground;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class BaseTest {
 
     public WebDriver driver;
 
+    String guru = "http://live.demoguru99.com/";
+
     @BeforeTest
     public void setBaseURL() {
-        FirefoxOptions capabilities = new FirefoxOptions();
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver(capabilities);
-        driver.get("http://automationpractice.com/index.php");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(guru);
+        driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
     }
 
     @AfterTest
     public void endSession() {
         driver.quit();
     }
-
 
 }
--- src/test/java/playground/SampleTest.java
+++ src/test/java/playground/SampleTest.java
@@ -1,10 +1,18 @@
 package playground;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-public class SampleTest extends BaseTest{
+import java.util.Random;
+
+
+public class SampleTest extends BaseTest {
+
 
     @Test
     public void goToWomensPage() {
@@ -14,5 +22,63 @@ public class SampleTest extends BaseTest{
         Assert.assertEquals(title, "My Store");
     }
 
-}
 
+
+    @Test
+    public void addProductToWishListTest() {
+        registerNewUser();
+        addProductToWishList();
+
+    }
+    @Test
+    public void registerNewUserTest(){
+
+        registerNewUser();
+    }
+
+    @Test
+    public void shareWishListTest(){
+        registerNewUser();
+        addProductToWishList();
+        shareWishList();
+
+    }
+
+    public void registerNewUser() {
+
+        String title = driver.getTitle();
+
+        driver.findElement(By.xpath("//*[@id=\"header\"]//*[@class=\"skip-links\"]//*[@data-target-element=\"#header-account\"]")).click();
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/div[1]/div[2]/a")).click();
+        driver.findElement(By.name("firstname")).sendKeys("zan");
+        driver.findElement(By.name("middlename")).sendKeys("keegan");
+        driver.findElement(By.name("lastname")).sendKeys("hendricks");
+        driver.findElement(By.name("middlename"));
+        driver.findElement(By.name("email")).sendKeys("testmail" + RandomStringUtils.randomNumeric(7) + "@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("chelsea");
+        driver.findElement(By.name("confirmation")).sendKeys("chelsea");
+        driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button")).click();
+
+        Assert.assertEquals(title, "Home page");
+    }
+
+    public void addProductToWishList() {
+        String title = driver.getTitle();
+        driver.findElement(By.xpath("//*[@id=\"nav\"]/ol/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[2]/ul/li[2]/div/div[3]/ul/li[1]/a")).click();
+    }
+
+    public void shareWishList(){
+        String title = driver.getTitle();
+        driver.findElement(By.xpath("//*[@id=\"wishlist-view-form\"]/div/div/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"email_address\"]")).sendKeys("chelslaguma");
+        driver.findElement(By.xpath("//*[@id=\"message\"]")).sendKeys("I love this tv we should buy it ");
+        driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button")).click();
+
+
+    }
+
+
+
+}
